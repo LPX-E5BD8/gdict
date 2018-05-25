7e4c81b2f356b409e68760fe602f9e7e28d61e52
lipengxiang <lipengxiang@xiaomi.com>
2018-05-25 20:48:14 +0800

    修改参数传入方式，提高使用便利度

--- gdict.go
+++ gdict.go
@@ -1,45 +1,72 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 
 	"github.com/liipx/gdict/engien"
 )
 
 func main() {
-	query := flag.String("q", "", "Queries.")
-	style := flag.String("S", "dark", "Color Style. [dark,light]")
-	say := flag.Bool("s", false, "Only used on MacOS.")
-	e := flag.String("-engine", "youdao", "Query engine, only youdao yet.")
+	// 朗读开关
+	say := false
+	style := "dark"
+	e := "youdao"
 
-	flag.Parse()
+	// 传入参数
+	query := os.Args[1]
 
-	if *query == "" {
-		fmt.Println("Useage: gdict -q 'lips'")
-		os.Exit(1)
-	}
-
-	// TODO more engines supported
-	switch *e {
-	default:
-		if len(os.Args) > 0 {
-			engien.NewYoudao(*query, *style).Query()
+	// 动态参数设置
+	startSetting := false
+	for _, arg := range os.Args[1:] {
+		switch strings.ToLower(arg) {
+		case "-h", "--help":
+			fmt.Sprintf("Options：\n\t配色类: -dark, -light" +
+				"\n\t语音朗读(限MacOS): -s, --say" +
+				"\n\t翻译引擎: -youdao" +
+				"\n\t帮助: -h, --help")
+		case "-black", "-light":
+			startSetting = true
+			style = strings.Replace(arg, "-", "", -1)
+		case "-s", "--say":
+			startSetting = true
+			say = true
+		case "-youdao":
+			startSetting = true
+			style = strings.Replace(arg, "-", "", -1)
+		default:
+			if !startSetting {
+				query += arg
+			} else {
+				fmt.Printf("\nUsage: \n\tgdict words ... [args ...]\n\t请将参数置于查询词之后")
+				return
+			}
 		}
 	}
 
-	if *say && runtime.GOOS == "darwin" {
+	if query == "" {
+		fmt.Println("Useage: gdict 'lips'")
+		os.Exit(1)
+	}
+
+	switch e {
+	default:
+		// TODO: More engines
+		engien.NewYoudao(query, style).Query()
+	}
+
+	if say && runtime.GOOS == "darwin" {
 		sayPath, err := exec.LookPath("say")
 		if err != nil {
 			log.Println(err)
 			os.Exit(1)
 		}
 
-		cmd := exec.Command(sayPath, *query)
+		cmd := exec.Command(sayPath, query)
 		cmd.Run()
 	}
 
