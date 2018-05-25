2a473832b05c8e2fcb8f8b6853593b4f80244a54
lipengxiang <lipengxiang@xiaomi.com>
2018-05-25 21:03:14 +0800

    更合理的help

--- gdict.go
+++ gdict.go
@@ -25,18 +25,20 @@ func main() {
 
 	// 传入参数
 	query := os.Args[1]
+	switch strings.ToLower(query) {
+	case "-h", "--help":
+		fmt.Printf("Options:\n%7s: -dark, -light"+
+			"\n%7s: -s, --say  (MacOS only)"+
+			"\n%7s: -youdao"+
+			"\n%7s: -h, --help \n", "Style", "Read", "Engine", "Help")
+		return
+	}
 
 	// 动态参数设置
 	startSetting := false
 	if len(os.Args) > 2 {
-		for _, arg := range os.Args[2:] {
+		for _, arg := range os.Args[1:] {
 			switch strings.ToLower(arg) {
-			case "-h", "--help":
-				fmt.Printf("Options:\n%7s: -dark, -light"+
-					"\n%7s: -s, --say  (MacOS only)"+
-					"\n%7s: -youdao"+
-					"\n%7s: -h, --help \n", "Style", "Read", "Engine", "Help")
-				return
 			case "-black", "-light":
 				startSetting = true
 				style = strings.Replace(arg, "-", "", -1)
